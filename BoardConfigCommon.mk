b4ac8adda02227864e88fae5eb6a1b86933ab081
Kevin F. Haggerty <haggertk@lineageos.org>
2018-08-26 04:31:00 +0200

    kirin970-common: Allow additional gralloc 1.0 buffer usage bits
    Change-Id: Ie5a8a46fef1c0f28ebd2cd3b3d29cc19763bc2a3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,6 +43,9 @@ BOARD_HAVE_BLUETOOTH := true
 # Extended Filesystem Support
 TARGET_EXFAT_DRIVER := exfat
 
+# Display
+TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x2080000U
+
 # Lineage hardware
 BOARD_HARDWARE_CLASS += \
     $(VENDOR_PATH)/lineagehw
