4e7131731ebf513cb8568ac3d3622ebc2b461cf2
moepda <moepda@gmail.com>
2018-09-20 22:20:04 +0100

    Restore telephony-ext
    -Fixes sim info fc
    -Settings/network&internet/sim cards
    
    Change-Id: Ic84f57a4b4ea03ac98920ae9905630dd19156a1d
    Signed-off-by: moepda <moepda@gmail.com>

--- kirin970.mk
+++ kirin970.mk
@@ -82,6 +82,13 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.version.sdk=$(PLATFORM_SDK_VERSION) \
     ro.cust.cdrom=/dev/null
 
+# Radio
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Recovery
 PRODUCT_PACKAGES += \
     resize2fs_static
